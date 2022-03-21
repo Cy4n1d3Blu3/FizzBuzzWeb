8be140f6024a5a4474d323d2a5e42b705d7300e0
IT <IT@DESKTOP-UIFIGN1>
2022-03-21 14:40:24 +0100

    Add project files.

---
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Display(Name = "NUMBER:")]
+        [Required, Range(1, 1000, ErrorMessage = "Required value is from range {1}-{2}.")]
+        public int? Number { get; set; }
+    }
+}
---
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using FizzBuzzWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
+        public string Name { get; set; }
+        public string FB { get; set; } = "";
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult OnPost()
+        {
+            FB = "";
+            if (FizzBuzz.Number % 3 == 0) FB += "Fizz";
+            if (FizzBuzz.Number % 5 == 0) FB += "Buzz";
+            if (FizzBuzz.Number % 3 != 0 && FizzBuzz.Number % 5 != 0) FB += "Number: " + FizzBuzz.Number.ToString() + " is not a FizzBuzz number.";
+            return Page();
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name)) Name = "User";
+        }
+    }
+}
